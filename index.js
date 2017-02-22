b36b767c73b0a3e7fcd7918cac754d9d3cb90c33
Kyle Lee <hello.kylelee@gmail.com>
2017-02-22 01:26:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 function createPost() {
-  var pageTemplate = document.getElementById("page-template").innerHTML;
-  var mainPage = document.getElementById('post-anchor');
-  var pageTemplateFn = _.template(pageTemplate);
-  pageTemplateHTML = pageTemplateFn();
-  mainPage += pageTemplate;
+  //create templates
+  var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  var postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+
+  // get blog values
   var postTitle = document.getElementById('postTitle').value;
   var author = document.getElementById('postAuthor').value;
   var postBody = document.getElementById('postBody').value;
-  var postTemplate = document.getElementById('post-template').innerHTML;
-  var postTemplateFn = _.template(postTemplate);
+
+  //append page template to main tag
+  document.getElementsByTagName('main')[0].innerHTML += pageTemplate;
+
+  //insert values from form into Lodash template + get post id to append to + create comments section
+  var postHTML = postTemplate( 'title': postTitle, 'postBodyTemp': postBody, 'authorTemp': author );
   var postSection = document.getElementById('post');
-  var postHTML = postTemplateFn({ 'title': postTitle, 'postBodyTemp': postBody, 'authorTemp': author })
+  var commentHTML = commentsTemplate();
+
+  //append post section to correct spot in DOM and do the same with the empty comments section
   postSection.innerHTML = postHTML;
+  document.getElementsByClassName('posted-by').innerHTML += commentHTML;  
 }
 
 function postComment() {
+  //create comment template
+  var commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
+
+  //receive values from comment form and store them in variables
   var commenter = document.getElementById('commenterName').value;
   var comment = document.getElementById('commenterText').value;
-  var commentTemplate = document.getElementById('comment-template').innerHTML;
-  var commentTemplateFn = _.template(commentTemplate);
+
+  //receive spot in DOM to place the comments
   var comments = document.getElementById('comments');
-  var commentHTML = commentTemplateFn({ 'comment': comment, 'commenter': commenter });
-  comments += commentHTML;
+
+  //fill in comment values dynamically via Lodash and append to comments section on one line
+  comments.innerHTML += commentTemplate({ 'comment': comment, 'commenter': commenter });
 }
