d1c59a8264217e0e2ee9405b3830af9d3b0e1f2e
Ines Pucker <ines.pucker@gmx.de>
2017-03-04 16:03:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-var i=0
-
-
 function createPost() {
-  var header = document.getElementById("HeaderText").value;
-  var post = document.getElementById("PostText").value;
-  var author = document.getElementById("AuthorName").value;
-  //nimmt alle Inhalte der Input-Boxen
+	 //nimmt alle Inhalte der Input-Boxen - das muss zuerst geschehen, sonst werden die Inhalt wieder gelöscht wenn man die pageDivs hinzufügt
+	var header = document.getElementById("postTitle").value;
+  var post = document.getElementById("postBody").value;
+  var author = document.getElementById("postAuthor").value;
 
-  //erzeugt einen Template-String
-   var postTemplate = document.getElementById("post-template").innerHTML;
-  //erzeugt die Funktion zu dem Template
-  var templateFn = _.template(postTemplate);
-	//erzeugt eine Variable zum DOM-Element mit der id="post"
-  var postsDiv = document.getElementById("post-form");
+   //erzeugt aus dem lodash Template das HTML-Template
+   var templateHTML = document.getElementById("page-template").innerHTML;
+
+
+   var pageDiv =  document.getElementsByTagName("main")[0]
+
+	 // fügt das HTML-Template zu dem DOM-Element
+	pageDiv.innerHTML += templateHTML;
+
+
+
+
+  //erzeugt einen Template-String und daraus dann gleich die Funktion zu dem Template
+   var templateFn= _.template(document.getElementById("post-template").innerHTML);
+   // page template
+
+  	//erzeugt eine Variable zum DOM-Element mit der id="post"
+	var postsDiv =  document.getElementById("post")
+
 
   //execute template function with JSON object for the interpolated values
   var templateHTML = templateFn({ 'header': header, 'post': post, 'author': author });
@@ -20,22 +30,43 @@ function createPost() {
   //append rather than replace!
   postsDiv.innerHTML += templateHTML;
 
+
+  //erzeugt einen Template-String und daraus dann gleich die Funktion zu dem Template
+   var templateFn= _.template(document.getElementById("comments-template").innerHTML);
+
+
+  	//erzeugt eine Variable zum DOM-Element mit der id="sidebar"
+	var commentsDiv =  document.getElementById("sidebar")
+
+	//erzeugt aus der Funktion das HTML-Template
+	var templateHTML = templateFn();
+
+  // fügt das HTML-Template zu dem DOM-Element
+	commentsDiv.innerHTML += templateHTML;
+
   }
 
-function postComment(i) {
 
 
-  var commenter = document.getElementById("commenterName"+i).value;
-  var comment = document.getElementById("commentText"+i).value;
+
+
+
+
+
+function postComment() {
+
+
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
   //insert comment into "comments" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
 
   //create template string
-   var commentTemplate = document.getElementById("comments-template").innerHTML;
+   var commentTemplate = document.getElementById("comment-template").innerHTML;
   //create template function
   var templateFn = _.template(commentTemplate);
 
-  var commentsDiv = document.getElementById(i);
+  var commentsDiv = document.getElementById("sidebar");
 
   //execute template function with JSON object for the interpolated values
   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
