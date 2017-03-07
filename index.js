6dbe1529c825ecbe151a63814b4c31ef6dbd3e22
Ines Pucker <ines.pucker@gmx.de>
2017-03-07 17:13:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 function createPost() {
+	//erzeugt  das Page-Template
+	var pageTemplate = document.getElementById("page-template").innerHTML;
+
+	//erzeugt einen Post-Template-String und daraus dann gleich die Funktion zu dem Template
+	var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+
+	//erzeugt einen Comments-Template-String und daraus dann gleich die Funktion zu dem Template
+	var commentsTemplate = document.getElementById("comments-template").innerHTML;
+
 	 //nimmt alle Inhalte der Input-Boxen - das muss zuerst geschehen, sonst werden die Inhalt wieder gelöscht wenn man die pageDivs hinzufügt
 	var header = document.getElementById("postTitle").value;
-  var post = document.getElementById("postBody").value;
-  var author = document.getElementById("postAuthor").value;
+	var post = document.getElementById("postBody").value;
+	var author = document.getElementById("postAuthor").value;
 
-   //erzeugt  das HTML-Template
-   var templateHTML = document.getElementById("page-template").innerHTML;
+	//Erzeugt das Template mit dem Inhalt aus den Input-Feldern
+	var postTemplate = postTemplate({ 'header': header, 'post': post, 'author': author });
 
 
-   var pageDiv =  document.querySelector('main')
+
+
+
+	//erzeugt eine Variable zum DOM-Element main
+    var pageDiv =  document.querySelector('main')
 
 	 // fügt das HTML-Template zu dem DOM-Element
-	pageDiv.innerHTML += templateHTML;
+	pageDiv.innerHTML += pageTemplate;
 
 
-
-
-  //erzeugt einen Template-String und daraus dann gleich die Funktion zu dem Template
-   var templateFn= _.template(document.getElementById("post-template").innerHTML);
-   // page template
 
   	//erzeugt eine Variable zum DOM-Element mit der id="post"
 	var postsDiv =  document.getElementById("post")
 
 
-  //execute template function with JSON object for the interpolated values
-  var templateHTML = templateFn({ 'header': header, 'post': post, 'author': author });
-
-  //append rather than replace!
-  postsDiv.innerHTML += templateHTML;
-
-
-  //erzeugt einen Template-String und daraus dann gleich die Funktion zu dem Template
-   var templateFn= _.template(document.getElementById("comments-template").innerHTML);
+    //fügt das Post-Template zum DOM DIV post
+	postsDiv.innerHTML += postTemplate;
 
 
   	//erzeugt eine Variable zum DOM-Element mit der id="sidebar"
 	var commentsDiv =  document.getElementById("sidebar")
 
-	//erzeugt aus der Funktion das HTML-Template
-	var templateHTML = templateFn();
 
-  // fügt das HTML-Template zu dem DOM-Element
-	commentsDiv.innerHTML += templateHTML;
+  // fügt das Comments-Template zu dem DOM-Element sidebar
+	commentsDiv.innerHTML += commentsTemplate;
 
   }
 
@@ -55,22 +54,22 @@ function createPost() {
 
 function postComment() {
 
+	//create template string
+	   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+	  //create template function
 
-  var commenter = document.getElementById("commenterName").value;
-  var comment = document.getElementById("commentText").value;
-  //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+	  var commenter = document.getElementById("commenterName").value;
+	  var comment = document.getElementById("commentText").value;
+	  //insert comment into "comments" div in this format:
+	  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
 
-  //create template string
-   var commentTemplate = document.getElementById("comment-template").innerHTML;
-  //create template function
-  var templateFn = _.template(commentTemplate);
 
-  var commentsDiv = document.getElementById("sidebar");
 
-  //execute template function with JSON object for the interpolated values
-  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+	  var commentsDiv = document.getElementById("comments");
 
-  //append rather than replace!
-  commentsDiv.innerHTML += templateHTML;
+	  //execute template function with JSON object for the interpolated values
+	  var commentTemplate = commentTemplate({ 'comment': comment, 'commenter': commenter });
+
+	  //append rather than replace!
+	  commentsDiv.innerHTML += commentTemplate;
 }
