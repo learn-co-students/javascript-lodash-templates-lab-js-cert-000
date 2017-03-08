7b34f6e151d58f2f4f2fd5d5041f4d9f3a13b630
Ines Pucker <ines.pucker@gmx.de>
2017-03-08 16:59:16 +0000

    Done.

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ function createPost() {
 	var pageTemplate = document.getElementById("page-template").innerHTML;
 
 	//erzeugt einen Post-Template-String und daraus dann gleich die Funktion zu dem Template
- 	var postTemplate = document.getElementById("post-template");
+	var postTemplate = document.getElementById("post-template");
 	var postTemplate = _.template(document.getElementById("post-template").innerHTML);
 
 	//erzeugt einen Comments-Template-String und daraus dann gleich die Funktion zu dem Template
 	var commentsTemplate = document.getElementById("comments-template").innerHTML;
+	
+	
+	 var postForm = document.getElementById("post-form")
 
 	 //nimmt alle Inhalte der Input-Boxen - das muss zuerst geschehen, sonst werden die Inhalt wieder gelöscht wenn man die pageDivs hinzufügt
 	var header = document.getElementById("postTitle").value;
 	var post = document.getElementById("postBody").value;
 	var author = document.getElementById("postAuthor").value;
-
+	
 	//Erzeugt das Template mit dem Inhalt aus den Input-Feldern
 	var postTemplate = postTemplate({ 'header': header, 'post': post, 'author': author });
-
-
-
-
-
+	
+	
+	
+	
+	
 	//erzeugt eine Variable zum DOM-Element main
     var pageDiv =  document.querySelector('main')
 
 	 // fügt das HTML-Template zu dem DOM-Element
-	pageDiv.innerHTML += pageTemplate;
+	pageDiv.innerHTML = pageTemplate;
+	
 
-
-
+	
   	//erzeugt eine Variable zum DOM-Element mit der id="post"
 	var postsDiv =  document.getElementById("post")
 
@@ -40,37 +43,42 @@ function createPost() {
   	//erzeugt eine Variable zum DOM-Element mit der id="sidebar"
 	var commentsDiv =  document.getElementById("sidebar")
 
-
+	
   // fügt das Comments-Template zu dem DOM-Element sidebar
 	commentsDiv.innerHTML += commentsTemplate;
+	
+	
 
   }
 
 
 
-
-
-
-
+  
+    
+  
+  //index functions createPost builds the proper templates:                                                                                                       
+    // Error: spy was never called with [ '\n      <div id="sidebar"></div>\n       <div id="post"></div>\n     ' ] 
+  
+  
 
 function postComment() {
+ //create template string
+   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  //create template function
+  
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
 
-	//create template string
-	   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
-	  //create template function
+ 
 
-	  var commenter = document.getElementById("commenterName").value;
-	  var comment = document.getElementById("commentText").value;
-	  //insert comment into "comments" div in this format:
-	  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+  var commentsDiv = document.getElementById("comments");
 
+  //execute template function with JSON object for the interpolated values
+  var commentTemplate = commentTemplate({ 'comment': comment, 'commenter': commenter });
 
-
-	  var commentsDiv = document.getElementById("comments");
-
-	  //execute template function with JSON object for the interpolated values
-	  var commentTemplate = commentTemplate({ 'comment': comment, 'commenter': commenter });
-
-	  //append rather than replace!
-	  commentsDiv.innerHTML += commentTemplate;
+  //append rather than replace!
+  commentsDiv.innerHTML += commentTemplate;
 }
+
