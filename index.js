f165a42e7c488601ec49e70d1970365c179ea407
Ines Pucker <ines.pucker@gmx.de>
2017-03-09 13:32:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ function createPost() {
 
 	//erzeugt einen Comments-Template-String und daraus dann gleich die Funktion zu dem Template
 	var commentsTemplate = document.getElementById("comments-template").innerHTML;
-	
-	
-	 var postForm = document.getElementById("post-form")
+
+
+
 
 	 //nimmt alle Inhalte der Input-Boxen - das muss zuerst geschehen, sonst werden die Inhalt wieder gelöscht wenn man die pageDivs hinzufügt
 	var header = document.getElementById("postTitle").value;
 	var post = document.getElementById("postBody").value;
 	var author = document.getElementById("postAuthor").value;
-	
+
 	//Erzeugt das Template mit dem Inhalt aus den Input-Feldern
 	var postTemplate = postTemplate({ 'header': header, 'post': post, 'author': author });
-	
-	
-	
-	
-	
+
+
+
+
+
 	//erzeugt eine Variable zum DOM-Element main
     var pageDiv =  document.querySelector('main')
 
 	 // fügt das HTML-Template zu dem DOM-Element
 	pageDiv.innerHTML = pageTemplate;
-	
 
-	
+
+
   	//erzeugt eine Variable zum DOM-Element mit der id="post"
 	var postsDiv =  document.getElementById("post")
 
@@ -43,35 +43,36 @@ function createPost() {
   	//erzeugt eine Variable zum DOM-Element mit der id="sidebar"
 	var commentsDiv =  document.getElementById("sidebar")
 
-	
+
   // fügt das Comments-Template zu dem DOM-Element sidebar
 	commentsDiv.innerHTML += commentsTemplate;
-	
-	
+
+
 
   }
+	
 
 
 
-  
-    
-  
-  //index functions createPost builds the proper templates:                                                                                                       
-    // Error: spy was never called with [ '\n      <div id="sidebar"></div>\n       <div id="post"></div>\n     ' ] 
-  
-  
+
+
+
+  //index functions createPost builds the proper templates:
+    // Error: spy was never called with [ '\n      <div id="sidebar"></div>\n       <div id="post"></div>\n     ' ]
+
+
 
 function postComment() {
  //create template string
    var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
   //create template function
-  
+
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
   //insert comment into "comments" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
 
- 
+
 
   var commentsDiv = document.getElementById("comments");
 
@@ -81,4 +82,3 @@ function postComment() {
   //append rather than replace!
   commentsDiv.innerHTML += commentTemplate;
 }
-
